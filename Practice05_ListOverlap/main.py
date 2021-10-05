c417ed07fa25239efa8e4bc3443cb4dc1438cd37
Ray Yan <ray.yan@chunwo.com>
2021-10-05 14:39:11 +0800

    Finish on practice 05 on Python

---
+++ Practice05_ListOverlap/main.py
@@ -0,0 +1,19 @@
+import random
+
+ranList01 = []
+ranList02 = []
+
+
+def ListOverLap():
+    ranList01 = range(1, random.randint(1, 50))
+    ranList02 = range(1, random.randint(1, 50))
+
+    print("Total leng of List 01 :" + str(ranList01))
+    print("Total leng of List 02 :" + str(ranList02))
+    for elem in ranList01:
+        if elem in ranList02:
+            print(elem)
+    print(elem + 1)  # since the last digi will be n-1, need to fill back 1 digi
+
+
+ListOverLap()
